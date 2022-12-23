2b60863db27deb16521a9b3f39ff07bc0afc6985
Valya Danilova <v.danilova@ongrid.pro>
2022-12-23 12:47:46 +0400

    add deploy script

---
+++ scripts/GLDToken_deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const GLDToken = await hre.ethers.getContractFactory("GLDToken");
+  console.log("Deploying GLDToken...");
+  const token = await GLDToken.deploy("10000000000000000000000");
+
+  await token.deployed();
+  console.log("GLDToken deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
