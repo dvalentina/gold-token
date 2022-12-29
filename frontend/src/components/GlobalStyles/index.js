87465a28c70f392c41f932ca3a004e4b20ab8680
Valya Danilova <v.danilova@ongrid.pro>
2022-12-29 15:09:08 +0400

    add global styles

---
+++ frontend/src/components/GlobalStyles/index.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: Verdana, sans-serif;
+    background: #f3f3f3;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyles;
