d5d9578af99cfab527306919cdcf04bfe1d37794
Valya Danilova <v.danilova@ongrid.pro>
2023-01-16 18:13:11 +0400

    add link to home to logo

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -4,6 +4,7 @@ import AboutAdmin from "../About/AboutAdmin";
 import GoerliLogo from "../GoerliLogo";
 import { LogoGrid, AboutGrid, AdminGrid } from "./Header.styled";
 import { TokenContext } from "../../contexts/TokenContext";
+import { Link } from "react-router-dom";
 
 function Header() {
   const { isAdmin } = useContext(TokenContext);
@@ -14,7 +15,9 @@ function Header() {
         <AboutProject />
       </AboutGrid>
       <LogoGrid>
-        <GoerliLogo />
+        <Link to="/">
+          <GoerliLogo />
+        </Link>
       </LogoGrid>
       {isAdmin ? (
         <AdminGrid>
